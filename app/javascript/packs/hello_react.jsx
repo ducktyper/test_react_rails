ac6daf787fce5d1ec142a5db5962b3a15bc21c00
Ducksan Cho <ducktyper@gmail.com>
2017-05-27 14:35:46 +1200

    Extract component to related folder

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,18 +5,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
+import Hello from '../hello/hello.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
